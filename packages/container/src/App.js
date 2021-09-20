929870a929e05b2c741b24a20d30d5b3b4a37ec5
IntoolabFront <t.gouidas@intoolab.com>
2021-09-20 16:56:12 +0300

    Created container deploy script 20

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there!s!</h1>
+      <h1>Hi there!!!</h1>
       <hr />
       <MarketingApp />
     </div>
