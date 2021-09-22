c55fbe533564b19a5dcd3a54fe209e056150fc34
IntoolabFront <t.gouidas@intoolab.com>
2021-09-22 14:10:42 +0300

    added auth subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, {lazy, Suspense, useState} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import MarketingApp from './components/MarketingApp';
+import Progress from './components/Progress';
 import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 })
 
 export default () => {
+  const [isSignIn, setIsSignIn] = useState(false)
   return (
     <BrowserRouter> 
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header onSignOut={() => setIsSignIn(false)} isSignedIn={isSignIn}/>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignIn(true)} />
+              </Route>
+              <Route path="/">
+                <MarketingLazy onSignIn={() => setIsSignIn(true)} />
+              </Route>
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
